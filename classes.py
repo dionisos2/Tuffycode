52b07ac1a65849875ca8e4f8688b3e5348fa293d
Curios7ty <58448067+Curios7ty@users.noreply.github.com>
2020-03-20 22:08:56 +0100

    feat: load endpoints

--- classes.py
+++ classes.py
@@ -1,25 +1,35 @@
 class Video:
     """Video class."""
-    def __init__(self, numId, size):
-        self.numId = numId
+    def __init__(self, num_id, size=1):
+        self.num_id = num_id
         self.size = size
-        self.requests = {}
-
-    def add_request(self, endpoint_id, nbr):
-        self.requests[endpoint_id] = nbr
-
+#        self.requests = {}
+#
+#    def add_request(self, endpoint_id, nbr):
+#        self.requests[endpoint_id] = nbr
+        
+    def set_size(self, size):
+        self.size = size
+    
     def __repr__(self):
-        return "Video({numId},{size})".format(numId=self.numId,size=self.size)
+        return "Video({num_id},{size})".format(num_id=self.num_id,size=self.size)
+
 
 class EndPoint:
     """EndPoint e.g. neighborhood."""
-    def __init__(self, numId, data_center_latency):
-        self.numId = numId
-        self.data_center_latency = data_center_latency
-        self.cache_latency = {}
+    def __init__(self, num_id, dc_latency):
+        self.num_id = num_id
+        self.dc_latency = dc_latency
+        self.caches_latency = dict()
 
-    def add_cache(self, cache_id, latency):
-        self.cache_latency[cache_id] = latency
+    def add_cache_latency(self, cache_id, latency):
+        self.caches_latency[cache_id] = latency
+
+    def __repr__(self):
+        return "EndPoint({num_id},{dc_latency},{c_latency})".format(\
+                                                num_id=self.num_id,\
+                                                dc_latency=self.dc_latency,\
+                                                c_latency=self.caches_latency)
 
 
 class Request:
@@ -29,13 +39,13 @@ class Request:
 
 
 class Cache:
-    def __init__ (self, numId, size):
-        self.numId = numId
+    def __init__ (self, num_id, size):
+        self.num_id = num_id
         self.size = size
         self.videosDict = set()
         
     def _add_video(self, video):
-        self.videosDict[video.numId] = video
+        self.videosDict[video.num_id] = video
 
 
 
--- load_problem.py
+++ load_problem.py
@@ -2,18 +2,51 @@ from classes import *
     #dio : lord il faut que tu mettes tous ça dans les objets classes associés.
     # et par contre n’hésite pas à dire si il y a un problème.
 
+def line_to_int_list(line):
+    return list(map(lambda x:int(x), line.split()))
+
+## == LOAD VIDEOS ==
 
 def load_videos(file_line):
     """ Return list of Video objects (numId, sz)"""
-    return list(map(lambda tup:Video(tup[0],int(tup[1])), enumerate(file_line.split())))
+    return list(map(lambda tup:Video(tup[0],tup[1]), enumerate(line_to_int_list(file_line))))
 
 # Test load_videos
-doTest = True
-if doTest:
+do_test_load_videos = False
+if do_test_load_videos:
     file_line = "50   50   80   30   110"
-    vidList = load_videos(file_line)
-    print(vidList)
-    
+    vid_list = load_videos(file_line)
+    print(vid_list)
+
+
+## == LOAD ENDPOINT ==
+
+def load_endpoint(file,num_endpoint):
+    """Return Endpoint object"""
+    # Create endpoint with data_center_latency
+    first_line = file.readline()
+    dc_lat, n_cache = line_to_int_list(first_line)
+    endpoint = EndPoint(num_endpoint,dc_lat)
+    # Add all linked caches, add latency
+    for _ in range(n_cache):
+        cur_line = file.readline()
+        cache_id, latency = line_to_int_list(cur_line)
+        endpoint.add_cache_latency(cache_id,latency)
+    return endpoint
+
+# Test load_endpoint
+do_test_load_endpoint = True
+if do_test_load_endpoint:
+    file_name = "test_load_endpoint.txt"
+    with open(file_name, "r+") as file:
+        # First endpoint
+        endpoint = load_endpoint(file,1)
+        print(endpoint)
+        # 2nd endpoint
+        endpoint2 = load_endpoint(file,2)
+        print(endpoint2)
+
+## == LOAD PROBLEM ==
 
 def load_problem(problem_path):
     """Load a problem file."""
@@ -34,7 +67,8 @@ def load_problem(problem_path):
         self.videos = load_videos(f.readline())
         # Next lines: endpoints (latency of DC and cache)
         for iE in range(self.inputs["E"]):
-            iE
+            self.endpoints.append(load_endpoint(f,iE))
+        # Next lines: 
 
     caches = [Cache(1)]
     endpoints = [EndPoint(1)]
