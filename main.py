54edff064fdf1ddc21b9cbe8b68a25449c66eca4
Sacha Ddkn <49188310+Sacha-Ddkn@users.noreply.github.com>
2020-03-20 20:00:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ from get_score import get_score
 
 class Problem:
     def __init__(self):
-        self.x=10
         self.result_path = None
 
         """ Load a problem file """
     def load_problem(self, problem_path):
-        pass
-
-
+        var = ["V","E","R","C","X"]
+        with open(problem_path, "r+") as f:
+            datas = f.readline()
+            datas = list(map(lambda x:int(x), datas.split()))
+            self.inputs = {var[i] : datas[i] for i in range(len(var))}
+            self.taille_videos = list(map(lambda x:int(x), f.readline().split()))
     """ Create a solution from the current problem and save it in a file"""
     def save_solution(self, result_path):
         self.result_path = result_path
         pass
-
 
     def get_score(self):
         return get_score(self.result_path)
