41ab0af6f097195c345df171b6c616adf7bda404
Kyrios-Vathys <62443394+Kyrios-Vathys@users.noreply.github.com>
2020-03-23 23:24:49 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ from get_score import get_score
 from load_and_save import load_problem, save_solution
 from create_solution import *
 
+#ceci est un test...
+
 def test_create_links_to_additions():
     problem_path = "./input/example.txt"
     # problem_path = "./input/me_at_the_zoo.in"
