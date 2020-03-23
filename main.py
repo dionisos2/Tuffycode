b632e54d1d55521af1dcd8cd0ea60b71879859d5
dionisos <denis.baudouin@gmail.com>
2020-03-23 22:55:07 +0100

    Create a random solution before the real one

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from load_and_save import load_problem, save_solution
 from create_solution import create_solution, create_random_solution
 
 
-def main(problem_name):
+def main(problem_name, random_solution = False):
     problem_path = f"./input/{problem_name}"
     solution_path = f"./output/{problem_name}"
 
     problem = load_problem(problem_path)
-    # solution = create_random_solution(problem)
-    solution = create_solution(problem)
+    if random_solution:
+        solution = create_random_solution(problem)
+    else:
+        solution = create_solution(problem)
     save_solution(solution, solution_path)
 
     score = get_score(problem_path, solution_path)
@@ -24,4 +26,12 @@ def test_example():
     print(score == 462500)
 
 test_example()
-main("me_at_the_zoo.in")
+
+def random_and_main(name):
+    print("random solution")
+    main(name, True)
+    print("talos solution")
+    main(name)
+
+
+random_and_main("kittens.in.txt")
