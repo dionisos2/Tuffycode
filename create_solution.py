79bc59dcb8965cd04ac0131f0ecb0866227ea71b
dionisos <denis.baudouin@gmail.com>
2020-03-23 20:12:50 +0100

    Implement add_video and get_additions_to_remove

--- create_solution.py
+++ create_solution.py
@@ -38,7 +38,7 @@ def create_solution(problem):
     while len(possible_additions) > 0:
         best_addition = get_best_additions(problem, possible_additions)
         add_video(problem, solution, best_addition)
-        additions_to_remove = get_additions_to_remove(solution, possible_additions, best_addition)
+        additions_to_remove = get_additions_to_remove(problem, solution, possible_additions, best_addition)
         recalculate_score(problem, possible_additions, links_to_additions, best_addition)
 
         for id_addition in additions_to_remove:
@@ -53,11 +53,11 @@ def create_possible_additions(problem):
     possible_additions = dict()
 
     for video in problem.videos:
-        video_id = video.num_id
-        for cache_id in problem.caches_id:
-            addition = Addition(video_id, cache_id)
-            addition.score = get_video_score(problem, video_id, cache_id)
-            possible_additions[(video_id, cache_id)] = addition
+        id_video = video.num_id
+        for id_cache in problem.caches_id:
+            addition = Addition(id_video, id_cache)
+            addition.score = get_video_score(problem, id_video, id_cache)
+            possible_additions[(id_video, id_cache)] = addition
     return possible_additions
 
 """Create a dict assotiating each cache to their connected endpoints"""
@@ -65,11 +65,11 @@ def create_cache_to_endpoints_link(problem):
     cache_to_endpoints = dict()
     for endpoint in problem.endpoints:
         connected_caches_id = endpoint.get_connected_caches_id()
-        for cache_id in connected_caches_id:
-            if cache_id in cache_to_endpoints:
-                cache_to_endpoints[cache_id].append(endpoint.num_id)
+        for id_cache in connected_caches_id:
+            if id_cache in cache_to_endpoints:
+                cache_to_endpoints[id_cache].append(endpoint.num_id)
             else:
-                cache_to_endpoints[cache_id] = [endpoint.num_id]
+                cache_to_endpoints[id_cache] = [endpoint.num_id]
     return cache_to_endpoints
 
 
@@ -82,23 +82,23 @@ def create_links_to_additions(problem):
     for video in problem.videos:
         result[video.num_id] = dict()
 
-        for current_cache_id in problem.caches_id:
+        for current_id_cache in problem.caches_id:
             connected_caches_id = set()
-            connected_endpoints = cache_to_endpoints[current_cache_id]
+            connected_endpoints = cache_to_endpoints[current_id_cache]
 
             for endpoint_id in connected_endpoints:
                 new_caches_id = problem.endpoints[endpoint_id].get_connected_caches_id()
                 connected_caches_id = connected_caches_id.union(new_caches_id)
 
-            result[video.num_id][current_cache_id] = []
-            for cache_id in connected_caches_id:
-                addition_id = (video.num_id, cache_id)
-                result[video.num_id][current_cache_id].append(addition_id)
+            result[video.num_id][current_id_cache] = []
+            for id_cache in connected_caches_id:
+                addition_id = (video.num_id, id_cache)
+                result[video.num_id][current_id_cache].append(addition_id)
 
     return result
 
 """Get the score of a video for a particular cache"""
-def get_video_score(problem, video_id, cache_id):
+def get_video_score(problem, id_video, id_cache):
     return 0
 
 """Return the current best possible additions (max of score/size)"""
@@ -115,17 +115,37 @@ def get_best_additions(problem, possible_additions):
 
 """Add a video to the correct cache of the solution"""
 def add_video(problem, solution, best_addition):
-    pass
+    id_video = best_addition.id_video
+    video = problem.videos[id_video]
+    id_cache = best_addition.id_cache
+
+    if id_cache in solution.caches:
+        solution.caches[id_cache].add_video(video)
+    else:
+        cache = Cache(id_cache)
+        cache.add_video(video)
+        solution.caches[id_cache] = cache
 
 """ recalculate the score of all possible additions that require modification"""
 def recalculate_score(problem, possible_additions, links_to_additions, best_addition):
-    additions_to_change = links_to_additions[best_addition.video_id][best_addition.cache_id]
+    additions_to_change = links_to_additions[best_addition.id_video][best_addition.id_cache]
 
     for addition_id in additions_to_change:
-        (video_id, cache_id) = addition_id
-        possible_additions[addition_id].score = get_video_score(problem, video_id, cache_id)
+        (id_video, id_cache) = addition_id
+        possible_additions[addition_id].score = get_video_score(problem, id_video, id_cache)
 
 """ Get a list of the id of all the additions to remove (for which the video would surcharge the size of the cache) """
-def get_additions_to_remove(solution, possible_additions, best_addition):
-    return []
+def get_additions_to_remove(problem, solution, possible_additions, best_addition):
+    addition_to_remove = []
+    max_size = problem.caches_size
+
+    for addition_id in possible_additions.keys():
+        id_video, id_cache = addition_id
+        video_size = problem.videos[id_video].size
+        current_size = sum(video.size for video in solution.caches[id_cache].videos)
+        if current_size + video_size > max_size:
+            addition_to_remove.append(addition_id)
+
+    return addition_to_remove
+
 
