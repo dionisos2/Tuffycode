61dc9585931aeb692c6e29687cc17b0d2c796edb
dionisos <denis.baudouin@gmail.com>
2020-03-27 19:40:37 +0100

    brute force solution

--- lulu_problem/board.py
+++ lulu_problem/board.py
@@ -1,3 +1,16 @@
-class board:
-    def __init__(self, n, m):
-        pass
+class Board:
+    def __init__(self, xmax, ymax):
+        self.matrix = [[0]*ymax for _ in range(xmax)]
+        self.xmax = xmax
+        self.ymax = ymax
+
+    def __repr__(self):
+        result = ""
+
+        for y in range(self.ymax):
+            for x in range(self.xmax):
+                result += f"|{self.matrix[x][y]:03d}|"
+            result += "\n"
+
+        return result
+
--- lulu_problem/brute_force_solution.py
+++ lulu_problem/brute_force_solution.py
@@ -1,3 +1,46 @@
+from board import *
+from copy import deepcopy
 
-def get_solution():
-    return []
+DMOVES = [[-3,0], [3,0], [0,3], [0,-3], [2,2], [2,-2], [-2,2], [-2,-2]]
+def possible_moves(board, current_pos):
+    result = []
+    x, y = current_pos
+    for dmove in DMOVES:
+        new_x = dmove[0]+x
+        new_y = dmove[1]+y
+        if ((0 <= new_x < board.xmax) and
+            (0 <= new_y < board.ymax) and
+            (board.matrix[new_x][new_y] == 0)):
+            result.append((new_x, new_y))
+    return result
+
+
+
+def get_solution(board, current_pos=None, step=1):
+    result = 0
+
+    if step == (board.xmax*board.ymax)+1:
+        print(board)
+        return 1
+
+    if current_pos == None:
+        for x in range(board.xmax):
+            for y in range(board.ymax):
+                new_board = deepcopy(board)
+                new_board.matrix[x][y] = step
+                result += get_solution(new_board, (x, y), step+1)
+
+    else:
+        for move in possible_moves(board, current_pos):
+            x, y = move
+            new_board = deepcopy(board)
+            new_board.matrix[x][y] = step
+            result += get_solution(new_board, move, step+1)
+
+    return result
+
+
+board = Board(4,6)
+print(get_solution(board))
+
+
